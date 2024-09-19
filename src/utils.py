c416e6ebeb251ff5199c5bfe08ea030cfd68b2b6
Ashraf Mohammed <98876115+AshrafMd-1@users.noreply.github.com>
2024-09-19 23:56:48 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,7 @@ def bulk_rolls(from_roll, to_roll):
     roll = []
     current_roll = from_roll
 
-    while current_roll != next_roll(to_roll) and len(roll) <= 80:
+    while current_roll != next_roll(to_roll) and len(roll) <= 40:
         roll.append(current_roll)
         current_roll = next_roll(current_roll)
 
